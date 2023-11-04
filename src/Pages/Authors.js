7b2e4ae8d4a8b0cdffa0c29014abda053948fb78
gyaank <gyaan1997@gmail.com>
2023-11-04 14:34:05 +0530

    Singular book page added

--- src/Pages/Authors.js
+++ src/Pages/Authors.js
@@ -1,30 +1,36 @@
 import React, { useState, useEffect } from "react";
 import dummyData from "../Configs/dummyData";
-// import { Typography } from "@mui/material";
-import { Card, CardContent, Typography,Link } from "@mui/material";
-
+import { Card, CardContent, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const AuthorsPage = () => {
   const [authors, setAuthors] = useState([]);
-
+  const navigate = useNavigate();
+console.log(navigate)
   useEffect(() => {
     setAuthors(dummyData);
   }, []);
 
+
+  const handleNavigate = (route) => {
+    navigate(route);
+  };
+
   return (
     <div>
-                  <Typography variant="h4">Pick your favourite Author!</Typography>
-    {authors.map((author) => (
-      <Link key={author.id} to={`/bookdetail/${author.id}`} style={{ textDecoration: 'none' }}>
-        <Card style={{ margin: '10px', maxWidth: '300px' }}>
+      <Typography variant="h4">Pick your favourite Author!</Typography>
+      {authors.map((author) => (
+        <Card
+          key={author.id}
+          onClick={() => handleNavigate(`/bookdetail/:${author.id}`)} 
+          style={{ margin: '10px', maxWidth: '300px', cursor: 'pointer' }}
+        >
           <CardContent>
             <Typography variant="h6">{author.author}</Typography>
-          
           </CardContent>
         </Card>
-      </Link>
-    ))}
-  </div>
+      ))}
+    </div>
   );
 };
 
--- src/Pages/BookDetail.js
+++ src/Pages/BookDetail.js
@@ -1,13 +1,41 @@
-// BookDetail.js
-import React from 'react';
 
-const BookDetail = ({book}) => {
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import dummyData from "../Configs/dummyData";
+const BookDetailPage = () => {
+  const { id } = useParams(); // Geting book ID from the route parameters
+  const [book, setBook] = useState(null);
+
+  useEffect(() => {
+    const selectedBook = dummyData.find((item) => item.id === Number(id));
+    setBook(selectedBook);
+  }, [id]);
+
+  if (!book) {
+    return <Typography variant="h6">Loading...</Typography>;
+  }
+
+  const { title, author, genre, description, coverImage } = book;
+
   return (
     <div>
-      Add your BookDetail content here
-      {book}
+      <Typography variant="h4">{title}</Typography>
+      <Card style={{ maxWidth: 400, margin: "20px" }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={coverImage} // Assuming coverImage is the URL of the cover image
+          alt={title}
+        />
+        <CardContent>
+          <Typography variant="h6">Author: {author}</Typography>
+          <Typography variant="subtitle1">Genre: {genre}</Typography>
+          <Typography variant="body1">Description: {description}</Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 };
 
-export default BookDetail;
+export default BookDetailPage;
